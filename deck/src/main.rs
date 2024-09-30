6f3ef8795e1555633aeb19927ccabcb19504304e
techarm <techarmdev@gmail.com>
2024-09-30 21:28:31 +0900

    struct, arrays, vectors, mutable, immutable

---
+++ deck/src/main.rs
@@ -0,0 +1,23 @@
+use std::vec;
+
+#[derive(Debug)]
+struct Deck {
+    cards: Vec<String>,
+}
+
+fn main() {
+    let suits = ["Hearts", "Spades", "Diamonds"];
+    let values = ["Ace", "Two", "Three"];
+
+    let mut cards = vec![];
+
+    for suit in suits {
+        for value in values {
+            let card = format!("{} of {}", suit, value);
+            cards.push(card);
+        }
+    }
+
+    let deck = Deck { cards };
+    println!("Here your deck: {:#?}", deck);
+}
