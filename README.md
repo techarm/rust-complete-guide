dcd4ec5015803604e46ceae5abceb7196e2f1b21
techarm <techarmdev@gmail.com>
2024-10-01 20:41:11 +0900

    feat: add shuffle method

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # rust-complete-guide
+
 Understand ownership, lifetimes, traits, generics, and much more through practical projects
+
+## Baisc
+
+- struct
+- #[derive(Debug)]
+- {:?}, {:#?}
+- arrays, vectors
+- mutable, immutable
+- impl struct (add a function to struct)
+  - associated functions (Class method)
+  - method, operates on a specific instance of a struct
+- implicit return
+- crate == package
+  - rust standard library
+    - included with every project without any additional install
+    - docs at: **doc.rust-lang.org/std**
+  - external creates
+    - have to be installed into our project with `cargo add <name>`
+    - create listing at: **crates.io**
+    - doc also at: **docs.rs**
+- use external modules use the 'crate' keyword
+- use internal modules use the 'mod' keyword
--- deck/src/main.rs
+++ deck/src/main.rs
@@ -1,23 +1,38 @@
 use std::vec;
 
+use rand::seq::SliceRandom;
+use rand::thread_rng;
+
 #[derive(Debug)]
 struct Deck {
     cards: Vec<String>,
 }
 
-fn main() {
-    let suits = ["Hearts", "Spades", "Diamonds"];
-    let values = ["Ace", "Two", "Three"];
+impl Deck {
+    fn new() -> Self {
+        let suits = ["Hearts", "Spades", "Diamonds"];
+        let values = ["Ace", "Two", "Three"];
 
-    let mut cards = vec![];
-
-    for suit in suits {
-        for value in values {
-            let card = format!("{} of {}", suit, value);
-            cards.push(card);
+        let mut cards = vec![];
+        for suit in suits {
+            for value in values {
+                let card = format!("{} of {}", suit, value);
+                cards.push(card);
+            }
         }
+
+        Deck { cards }
     }
 
-    let deck = Deck { cards };
+    fn shuffle(&mut self) {
+        let mut rng = thread_rng();
+        self.cards.shuffle(&mut rng);
+    }
+}
+
+fn main() {
+    let mut deck = Deck::new();
+    deck.shuffle();
+
     println!("Here your deck: {:#?}", deck);
 }
